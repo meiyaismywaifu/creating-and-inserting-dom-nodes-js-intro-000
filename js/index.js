2d6201c556d7bdf2b634d55ffc57f4d24594d175
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-20 20:59:09 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+// did anything even change here if it was all done in browser console?
+// i don't see anything here or in the HTML file.
